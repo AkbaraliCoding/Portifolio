d14e289264b39eb7c7fa08d1c9262a279d41b977
akbarali <nematjonovakbarali19@gmail.com>
2023-12-16 23:48:33 -0800

    My Portfolio

---
+++ contact/script.js
@@ -0,0 +1,13 @@
+let maindiv = document.querySelector('#con')
+let fullname = document.querySelector('#fullname')
+let mbnumber = document.querySelector('#mbnumber')
+let email = document.querySelector('#email')
+let emailpass = document.querySelector('#emailpass')
+let msg = document.querySelector('#msg')
+let submitbtn = document.querySelector('#submit')
+
+submitbtn.addEventListener('click', function (e) {
+    e.preventDefault(true)
+} )
+
+
